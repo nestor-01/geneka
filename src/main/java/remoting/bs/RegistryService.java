9791b06fbf65139ed09d1a7b7e2b16d9721f74f5
nestor-01 <Nestor@Nestor-PC.home>
2015-04-19 17:20:02 -0500

    added Spring mvc and spring orm

---
+++ src/main/java/remoting/bs/RegistryService.java
@@ -0,0 +1,7 @@
+package remoting.bs;
+
+public interface RegistryService
+{
+	public void registerService(String serviceName, String serviceURL, int registryPort);
+	public String getService(String serviceName);
+}
---
+++ src/main/java/remoting/bs/RegistryServiceImpl.java
@@ -0,0 +1,25 @@
+package remoting.bs;
+import java.util.HashMap;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+
+public class RegistryServiceImpl implements RegistryService
+{
+	Log logger = LogFactory.getLog(getClass());
+	
+	private HashMap<String,String> hmServiceRegistry = new HashMap<String,String>();
+	
+	public void registerService(String serviceName, String serviceURL, int registryPort)
+	{
+		hmServiceRegistry.put(serviceName, serviceURL+":"+registryPort);
+		logger.info("RegistryService: registerService: registered service:"+serviceName+" at "+serviceURL);
+	}
+
+	public String getService(String serviceName)
+	{
+		return hmServiceRegistry.get(serviceName);
+	}
+
+}
