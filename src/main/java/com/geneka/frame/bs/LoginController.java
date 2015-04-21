d5924a0bfe57e341337e661d7db0e10282ef4518
nestor-01 <Nestor@Nestor-PC.home>
2015-04-20 20:52:01 -0500

    configurations spring

--- src/main/java/com/geneka/frame/bs/LoginController.java
+++ src/main/java/com/geneka/frame/bs/LoginController.java
@@ -4,10 +4,14 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 
@@ -19,6 +23,15 @@ public class LoginController {
 	
 	@RequestMapping(value = "/geneka/", method = RequestMethod.GET)
 	public String openApp(@PathVariable String appBeanId, Locale locale,
+			Model model) {
+		logger.info("Returning hello view");
+		return "index.html";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseStatus(value = HttpStatus.OK)
+	@ResponseBody
+	public String open(@PathVariable String appBeanId, Locale locale,
 			org.springframework.ui.Model model) {
 		logger.info("Returning hello view");
 		return "index.html";
---
+++ src/main/java/com/geneka/frame/ws/LoginService.java
@@ -0,0 +1,5 @@
+package com.geneka.frame.ws;
+
+public class LoginService {
+
+}
